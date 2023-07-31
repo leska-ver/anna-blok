b55f434a5721cb7fae4b66e4e2cd3151f84faea2
leska-ver <lyaisan.333@yandex.ru>
2023-07-31 21:22:28 +0500

    frontendblok.com

---
+++ Старт в веб-разработку/4.Добавим скрипты/4.Добавим скрипты/cats/js/script.js
@@ -0,0 +1,88 @@
+// Names - Имя
+const nameCats = [
+  'Кот с усами',
+  'Кошка с колпаком',
+  'Кот с тыквой',
+  'Кошка-балерина'
+]
+
+// Images - Картинка(img)
+const imageCats = [
+  'img/cats/01.png',
+  'img/cats/02.png',
+  'img/cats/03.png',
+  'img/cats/04.png',
+]
+
+
+// List - Инфо-список
+const infoCats = [
+  { 
+      type: "Гладкошерстный", 
+      color: "Белый с рыжими глазами", 
+      vaccine: "Вакцинирована",
+      passport: "Есть ветпаспорт",
+      things: "Приучен к когтеточке",
+      tray: "Знает лоточек"
+  },
+  { 
+      type: "Гладкошерстный", 
+      color: "Трёхцветная с зелеными глазами", 
+      vaccine: "Вакцинирована",
+      passport: "Есть ветпаспорт",
+      things: "Приучен к когтеточке",
+      tray: "Знает лоточек"
+  },
+  { 
+      type: "Гладкошерстный", 
+      color: "Черно-белый с зелеными глазами", 
+      vaccine: "Вакцинирована",
+      passport: "Есть ветпаспорт",
+      things: "Приучен к когтеточке",
+      tray: "Знает лоточек"
+  },
+  { 
+      type: "Гладкошерстный", 
+      color: "Трёхцветная с зелеными глазами", 
+      vaccine: "Вакцинирована",
+      passport: "Есть ветпаспорт",
+      things: "Приучен к когтеточке",
+      tray: "Знает лоточек"
+  },
+];
+
+
+
+// Names - Имя
+for (let i = 0; i < nameCats.length; i++) {
+  const dataNameCats = document.createTextNode(nameCats[i]);
+  const textCats = document.getElementById(`name-cat-${i+1}`);
+  textCats.appendChild(dataNameCats);
+}
+
+
+// Images - Картинка(img)
+const imagePaths = document.querySelectorAll('.thumb img')
+
+for (let i = 0; i < imagePaths.length; i++) {
+  imagePaths[i].src = imageCats[i];
+  imagePaths[i].alt = nameCats[i];
+}
+
+
+// List - Инфо-список
+for (let i = 0; i < infoCats.length; i++) {
+  const cat = infoCats[i];
+  const infoList = `
+    <ul>
+      <li>${cat.type}</li>
+      <li>${cat.color}</li>
+      <li>${cat.vaccine}</li>
+      <li>${cat.passport}</li>
+      <li>${cat.things}</li>
+      <li>${cat.tray}</li>
+    </ul>
+  `;
+  const targetElement = document.querySelector(`#name-cat-${i+1}`);
+  targetElement.insertAdjacentHTML('afterend', infoList);
+}
